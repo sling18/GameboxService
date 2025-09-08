dd29929c79d71b0d6732b58938a27306e890906d
DESKTOP-GDBHCCP\Danny <jose.arias38851@ucaldas.edu.co>
2025-09-08 14:11:14 -0500

    v1.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GameboxService/',
+  base: process.env.NODE_ENV === 'production' && process.env.RENDER ? '/' : '/GameboxService/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  preview: {
+    port: 4173,
+    host: true
+  }
 })
